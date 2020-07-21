be4c8b425355f0866317d93529ace4641ec66234
ddolacki@vub.hr <Princeskrafna226>
2020-07-21 10:23:11 +0200

    Dialogflow window

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import ChatBot from 'react-simple-chatbot';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <ChatBot
-           steps={[
-    {
-         id: 'hello-world',
-         message: 'Hello World!',
-         end: true,
-    },
-  ]}
-/>
+        <div>
+          <iframe
+            allow="microphone;"
+            width="350"
+            height="430"
+            src="https://console.dialogflow.com/api-client/demo/embedded/8fcaad77-9788-45c7-85cb-2d25b051ec7c">
+          </iframe>
+        </div>
+
+        {/* <ChatBot
+          steps={[
+            {
+              id: '1',
+              message: 'What is your name?',
+              trigger: '2',
+            },
+            {
+              id: '2',
+              user: true,
+              trigger: '3',
+            },
+            {
+              id: '3',
+              message: 'Hi {previousValue}, nice to meet you!',
+              end: true,
+            },
+          ]}
+        /> */}
+
 
       </header>
     </div>
